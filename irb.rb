8faf6752078165855ac053469b6531a0c6194115
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-07-17 16:59:01 -0400

    rename and update morse decode codes

--- irb.rb
+++ irb.rb
@@ -1,22 +1,47 @@
-2+2
-10/3
-10/3.0
-"Happy" + "coding"
-"Happy" + "\n" +  "coding"
-print "Happy" + "\n" +  "coding" + "\n"
+class MorseCodeDecoder
+  MORSE_CODE = {
+    '.-'    => 'A',
+    '-...'  => 'B',
+    '-.-.'  => 'C',
+    '-..'   => 'D',
+    '.'     => 'E',
+    '..-.'  => 'F',
+    '--.'   => 'G',
+    '....'  => 'H',
+    '..'    => 'I',
+    '.---'  => 'J',
+    '-.-'   => 'K',
+    '.-..'  => 'L',
+    '--'    => 'M',
+    '-.'    => 'N',
+    '---'   => 'O',
+    '.--.'  => 'P',
+    '--.-'  => 'Q',
+    '.-.'   => 'R',
+    '...'   => 'S',
+    '-'     => 'T',
+    '..-'   => 'U',
+    '...-'  => 'V',
+    '.--'   => 'W',
+    '-..-'  => 'X',
+    '-.--'  => 'Y',
+    '--..'  => 'Z'
+  }.freeze
 
-a = 3
-a
-a = "Hello"
-a
-b = "World!"
-puts(a + b)
-
-def hello(name)
-   puts("Hello #{name}!")
- end
- hello("David")
-
- def add(a,b)
-    puts(a + b)
+  def self.decode_char(morse_code)
+    MORSE_CODE[morse_code].upcase
   end
+
+  def self.decode_word(morse_word)
+    morse_word.split(' ').map { |char| decode_char(char) }.join
+  end
+
+  def self.decode(message)
+    message.split('   ').map { |word| decode_word(word) }.join(' ')
+  end
+end
+
+# Example usage
+message = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+decoded_message = MorseCodeDecoder.decode(message)
+puts decoded_message
